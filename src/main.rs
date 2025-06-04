65f21b76bc5c941de74785613cddce89ad2f3ede
Joan Antoni RE <joanantoni.re16@gmail.com>
2025-06-04 21:10:19 +0200

    Add controller two to main

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     let mut nes = Nes::default();
     nes.setup_tv();
     nes.connect_controller_one(ControllerButtons::default());
+    nes.connect_controller_two(ControllerButtons::default());
     // let cartidge = Cartidge::new(Path::new("/path/to/cartidge"));
     let cartidge = Cartidge::new("roms/Super Mario Bros. (World).nes");
     // let cartidge = Cartidge::new("roms/Galaga - Demons of Death (USA).nes");
